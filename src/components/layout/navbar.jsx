ffb4582ce43077ddcabc6571368065f00fc0b273
ousseynoukone <ousseynou781227@gmail.com>
2026-01-14 11:47:13 +0100

    feat: implement NavBar component with navigation

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,4 +1,14 @@
 
 export default function NavBar() {
+
+  return (
+    <nav className="bg-gray-800 p-4 text-white">
+      <ul className="flex space-x-4">
+        <li><a href="/home" className="hover:underline">Home</a></li>
+        <li><a href="/about" className="hover:underline">About</a></li>
+        <li><a href="/contact" className="hover:underline">Contact</a></li>
+      </ul>
+    </nav>
+  )
   
 }
