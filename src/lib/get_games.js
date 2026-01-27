fc1ed77e6e496e3b524a2984d71c66dc95491274
KosaAifet <killian.gautheur@gmail.com>
2026-01-27 15:48:46 +0100

    Ajout de getGamesByString

--- src/lib/get_games.js
+++ src/lib/get_games.js
@@ -3,67 +3,88 @@ import { useRef } from "react";
 let cacheAllGames = null;
 const api = "https://free-to-play-games-database.p.rapidapi.com/api";
 const headers = {
-    "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-    "x-rapidapi-key": "8ad313d678msh75b549a9788401dp1746d2jsn218aa5b681d9"
+  "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+  "x-rapidapi-key": "8ad313d678msh75b549a9788401dp1746d2jsn218aa5b681d9",
 };
 const size = 20;
 
 function getAllGames(API) {
-    return fetch(API + "/games", { headers })
-        .then((response) => response.json())
-        .then((data) => data)
-        .catch((error) => console.error("Error fetching games:", error));
+  return fetch(API + "/games", { headers })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error fetching games:", error));
 }
 
 export async function getGames(startIndex) {
-    // Si le cache est vide, on remplit la variable globale
-    if (!cacheAllGames) {
-        cacheAllGames = await getAllGames(api);
-    }
+  // Si le cache est vide, on remplit la variable globale
+  if (!cacheAllGames) {
+    cacheAllGames = await getAllGames(api);
+  }
 
-    if (!cacheAllGames || cacheAllGames.length === 0) {
-        return [];
-    }
+  if (!cacheAllGames || cacheAllGames.length === 0) {
+    return [];
+  }
 
-    // Extraction des jeux
-    const games = cacheAllGames.slice(startIndex, startIndex + size);
-    return games;
+  // Extraction des jeux
+  const games = cacheAllGames.slice(startIndex, startIndex + size);
+  return games;
 }
-
-
 
 export async function getGameById(id) {
-    const apibyId = api + "/game?id=" + id;
-    return fetch(apibyId, { headers })
-        .then((response) => response.json())
-        .then((data) => data)
-        .catch((error) => console.error("Error fetching game by ID:", error));
+  const apibyId = api + "/game?id=" + id;
+  return fetch(apibyId, { headers })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error fetching game by ID:", error));
 }
 
-export async function getGamesByName(name) {
-    const allGames = cacheAllGames || await getAllGames(api);
-    const id = allGames.find(game => game.title.toLowerCase() === name.toLowerCase())?.id;
+// export async function getGamesByName(name) {
+//   const allGames = cacheAllGames || (await getAllGames(api));
+//   const id = allGames.find(
+//     (game) => game.title.toLowerCase() === name.toLowerCase()
+//   )?.id;
 
-    if (id) {
-        return getGameById(id);
-    } else {
-        return null;
+//   if (id) {
+//     return getGameById(id);
+//   } else {
+//     return null;
+//   }
+// }
+
+export async function getGamesByString(str) {
+  const allGames = cacheAllGames || (await getAllGames(api));
+  const allGamesByString = [];
+
+  allGames.array.forEach((game) => {
+    const nameGame = game.title;
+    if (nameGame.includes(str)) {
+      allGamesByString.push(game);
     }
+  });
+
+  if (allGamesByString.count > 0) {
+    return allGamesByString;
+  } else {
+    return null;
+  }
 }
 
 export function getByArg(genre = [], platform = null, sortBy = null) {
-    let apiUrl = api + "/games";
-    if (genre.length !== 0) {
-        apiUrl += "?category=" + genre.join("&?category=");
-    }
-    if (platform) {
-        apiUrl += (genre.length === 0 ? "?" : "&?") + "platform=" + platform;
-    }
-    if (sortBy) {
-        apiUrl += (genre.length === 0 && !platform ? "?" : "&?") + "sort-by=" + sortBy;
-    }
-    return fetch(apiUrl, { headers })
-        .then((response) => response.json())
-        .then((data) => data)
-        .catch((error) => console.error("Error fetching games by arguments:", error));
+  let apiUrl = api + "/games";
+  if (genre.length !== 0) {
+    apiUrl += "?category=" + genre.join("&?category=");
+  }
+  if (platform) {
+    apiUrl += (genre.length === 0 ? "?" : "&?") + "platform=" + platform;
+  }
+  if (sortBy) {
+    apiUrl +=
+      (genre.length === 0 && !platform ? "?" : "&?") + "sort-by=" + sortBy;
+  }
+  return fetch(apiUrl, { headers })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) =>
+      console.error("Error fetching games by arguments:", error)
+    );
 }
