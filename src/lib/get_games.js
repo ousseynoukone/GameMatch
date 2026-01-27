7cb96796ea058c654b51d7c20078bb0ce79acaea
Quang quan <leo.langlois2@gmail.com>
2026-01-27 11:37:00 +0100

    feat: implement game fetching by ID, name, and additional arguments

--- src/lib/get_games.js
+++ src/lib/get_games.js
@@ -26,3 +26,41 @@ export async function getGames(startIndex, genre = [], platform = null, sort = n
     const games = cacheAllGames.slice(startIndex, startIndex + size);
     return games;
 }
+
+
+
+export async function getGameById(id) {
+    const apibyId = api + "/game?id=" + id;
+    return fetch(apibyId, { headers })
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => console.error("Error fetching game by ID:", error));
+}
+
+export async function getGamesByName(name) {
+    const allGames = cacheAllGames || await getAllGames(api);
+    const id = allGames.find(game => game.title.toLowerCase() === name.toLowerCase())?.id;
+
+    if (id) {
+        return getGameById(id);
+    } else {
+        return null;
+    }
+}
+
+export function getByArg(genre = [], platform = null, sortBy = null) {
+    let apiUrl = api + "/games";
+    if (genre.length !== 0) {
+        apiUrl += "?category=" + genre.join("&?category=");
+    }
+    if (platform) {
+        apiUrl += (genre.length === 0 ? "?" : "&?") + "platform=" + platform;
+    }
+    if (sortBy) {
+        apiUrl += (genre.length === 0 && !platform ? "?" : "&?") + "sort-by=" + sortBy;
+    }
+    return fetch(apiUrl, { headers })
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => console.error("Error fetching games by arguments:", error));
+}
