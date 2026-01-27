b5f472f902e16101b4de5eb5f687bbdab647f115
Quang quan <leo.langlois2@gmail.com>
2026-01-27 10:56:32 +0100

    fix: update API endpoints and add headers for game fetching

--- src/lib/get_games.js
+++ src/lib/get_games.js
@@ -1,11 +1,15 @@
 import { useRef } from "react";
 
 let cacheAllGames = null;
-const api = "api/games";
+const api = "https://free-to-play-games-database.p.rapidapi.com/api";
+const headers = {
+    "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+    "x-rapidapi-key": "8ad313d678msh75b549a9788401dp1746d2jsn218aa5b681d9"
+};
 const size = 20;
 
 function getAllGames(API) {
-    return fetch(API)
+    return fetch(API + "/games", { headers })
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.error("Error fetching games:", error));
@@ -27,8 +31,8 @@ export async function getGames(startIndex) {
 }
 
 export async function getGameById(id) {
-    apibyId = "api/game?id=" + id
-    return fetch(apibyId)
+    const apibyId = api + "/game?id=" + id;
+    return fetch(apibyId, { headers })
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.error("Error fetching game by ID:", error));
--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://www.freetogame.com",
-      },
-    },
-  },
 })
